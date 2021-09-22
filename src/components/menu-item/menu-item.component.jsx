ad496901321fc41475b68c782b0cbac05a2033c8
chanhminh99 <chungminhchanh1999@gmail.com>
2021-09-23 01:11:27 +0700

    Update our menu-items to navigate to another page  withRouter HOC

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-
+import { withRouter } from 'react-router'
 import './menu-item.styles.scss'
 
-const MenuItem = ({title, imageUrl, size}) => {
+const MenuItem = ({title, imageUrl, size, linkUrl, history, match}) => {
+    console.log('match', `${match.url}${linkUrl}`);
     return (
         <div
-            className={size ? `menu-item ${size}` : 'menu-item'}>
+            className={size ? `menu-item ${size}` : 'menu-item'}
+            onClick={() => history.push(`${match.url}${linkUrl}`)}
+        >
             <div
                 style={{backgroundImage: `url(${imageUrl})`}}
                 className='background-image' />
@@ -17,4 +20,4 @@ const MenuItem = ({title, imageUrl, size}) => {
     )
 }
 
-export default MenuItem;
+export default withRouter(MenuItem);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 
+const Root = () => {
+  return (
+    <Router>
+      <App />
+    </Router>
+  )
+}
 
 ReactDOM.render(
-  <App />,
+  <Root />,
   document.getElementById('root')
 );
 
