754d5c5e62837276d2e4e0d3ca5ddfe480f53765
chanhminh99 <chungminhchanh1999@gmail.com>
2021-10-02 22:42:44 +0700

    Added persist state in cart reducer with redux-persist

--- src/index.js
+++ src/index.js
@@ -2,17 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom';
-
-import store from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react';
+import wrapperStore from './redux/store'
 
 import './index.css';
 import App from './App';
 
 const Root = () => {
   return (
-    <Provider store={store}>
+    <Provider store={wrapperStore.store}>
       <Router>
+        <PersistGate persistor={wrapperStore.persistor} loading={null}>
         <App />
+        </PersistGate>
       </Router>
     </Provider>
   )
