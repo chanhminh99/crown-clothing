41621e2d74072da03a1eb76a48c18d9693685dbd
chanhminh99 <chungminhchanh1999@gmail.com>
2021-10-02 14:34:32 +0700

    Added reselect lib for selector state in redux, helping reduce re-rendering when some states change

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import { selectCartItems } from '../../redux/cart/cart.selectors'
+
 import CartItem from '../cart-item/cart-item.component'
 
 import CustomButton from '../custom-button/custom-button.component'
@@ -17,8 +19,8 @@ const CartDropDown = ({cartItems}) => {
     )
 }
 
-const mapStateToProps = ({cart: {cartItems}}) => ({
-    cartItems
+const mapStateToProps = (state) => ({
+    cartItems: selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropDown)
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,19 +5,25 @@ import {connect} from 'react-redux'
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
 import { toggleCartHidden } from '../../redux/cart/cart.action'
 
+import {selectCartItemsCount} from '../../redux/cart/cart.selectors'
+
 import './cart-icon.styles.scss'
 
-const CartIcon = ({toggleCartHidden = () => {}}) => {
+const CartIcon = ({toggleCartHidden = () => {}, currentTotalItem}) => {
     return (
         <div className='cart-icon' onClick={toggleCartHidden}>
             <ShoppingIcon className='shopping-icon' />
-            <span className='item-count'>0</span>
+            <span className='item-count'>{currentTotalItem}</span>
         </div>
     )
 }
+
+const mapStateToProps = (state) => ({
+    currentTotalItem: selectCartItemsCount(state)
+})
 
 const mapDispatchToProps = (dispatch) => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon)
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon)
