e1df9a5ef48ce749733f0198ba1dd1d9040855a7
chanhminh99 <chungminhchanh1999@gmail.com>
2021-10-19 22:03:18 +0700

    Add compression lib for optimize file when server send out to browser

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-
+const compression = require('compression')
 const path = require('path')
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config()
@@ -10,6 +10,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(compression())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
